0d7d4b23e1eb2e493fb645d02c95ddde8443bce1
Chad Bonthuys <chadb@sidpayment.com>
2019-11-08 10:08:24 +0200

    -Data layer and design time factory

---
+++ Checkout/Checkout.Data/CheckoutDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Checkout.Data
+{
+    public class CheckoutDBContext : DbContext
+    {
+        /// <summary>
+        /// Database Context for Entity Framework
+        /// </summary>
+        public CheckoutDBContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        #region DBSets
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Checkout/Checkout.Data/DesignTimeDBContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Checkout.Data
+{
+    public class DesignTimeDBContextFactory : IDesignTimeDbContextFactory<CheckoutDBContext>
+    {
+        public CheckoutDBContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<CheckoutDBContext>();
+            builder.UseSqlServer("Server=");
+
+            return new CheckoutDBContext(builder.Options);
+        }
+    }
+}
